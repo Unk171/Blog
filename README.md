2e03925d3a07d5c5367157f3bac511633a2416b3
Dmitry Dmitry <7353687@gmail.com>
2023-09-11 22:17:26 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Blog
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  ## Description
+  This is a blog with posts and comments. Blog is for autorized users only
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Iicense](#Iicense)
+- [How to contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+npm i
+## Usage
+Start mysql server and run server.js
+
+  ![Screenshot](./assets/images/screenshot14.png)
+## Iicense
+None
+## How to contribute
+N/A
+## Tests
+N/A/
+## Questions
+* Github: https://github.com/Unk171
+* You can send your questions to my e-mail: 7353687@gmail.com
+* Heroku: https://floating-gorge-35639-a29f57abc72e.herokuapp.com/
+  
--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,21 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-
 let sequelize;
 
-// if (some.env.variable) {
-//  sequelize = new Sequelize(
-//     "mysql://doubxdcgs23kzlta:yrbf91if3s49bzjt@acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ta0zjoyuvkbtqo4a"
-//   );
-// } else {
-
-  if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-      }
-    );
-  }
-// }
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
